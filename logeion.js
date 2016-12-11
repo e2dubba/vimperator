1f666882c39defa004ada775211b6c812aed9ee2
e2dubba <svirgilgoodwin@gmail.com>
2016-12-11 07:27:09 -0600

    made sure that logeion opens in a new tab

--- logeion.js
+++ logeion.js
@@ -6,9 +6,8 @@ commands.addUserCommand(['logeion'], "Search Logieon",
     function(args) {
         var url = "http://logeion.uchicago.edu/index.html#";
         url += encodeURIComponent(args.string);
-        liberator.echo(url)
-        var win = window.open(url, '_blank');
-        // commands.get("tabopen").action(url);
-        win.focus();
+        liberator.echo(url);
+	liberator.open(url,
+		liberator[options.get("activate").has("paste") ? "NEW_TAB" : "NEW_BACKGROUND_TAB"]);
     }
 );
