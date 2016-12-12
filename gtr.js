eec9b44392eaaefdbfb9cca4df0557ef9a20d556
e2dubba <svirgilgoodwin@gmail.com>
2016-12-11 19:54:39 -0600

    change file names

---
+++ gtr.js
@@ -0,0 +1,15 @@
+// Vimperator Plugin: 'gtr'
+// Usuage: use :gtr
+
+commands.addUserCommand(['gtr'], 'Opens Clipboard text in Google Translae', 
+	function () {
+        let otext = util.readFromClipboard()
+        otext = otext.replace(/\-\r?\n/g, '');
+        otext = otext.replace(/\r?\n/g, ' ');
+		var url = "https://translate.google.com/#auto/en/" ;
+	    url += encodeURIComponent(otext);
+        liberator.open(url,
+            liberator[options.get("activate").has("paste") ? "NEW_TAB" : "NEW_BACKGROUND_TAB"]);
+
+    }
+);
