168914e5c59830a3f85dc4a5e318388a0f055cc0
e2dubba <svirgilgoodwin@gmail.com>
2016-12-15 16:49:59 -0600

    fixed nodeyank

--- nodeyank.js
+++ nodeyank.js
@@ -2,14 +2,15 @@
 // Usuage: use :nodeyank
 
 commands.addUserCommand(['nodeyank'], 'Yanks the node from a url', 
-	function () {
-		var url = buffer.URL;
-		if (options.get("yankencodedurl").value)
-			url = services.get("io").newURI(url, buffer.charser, null).asciiSpec;
-		else
-			url = services.get("textToSubURI").unEscapeURIForUI(buffer.charset, url).replace(/ /g, "%20");
-		
-		util.copyToClipboard(url, true);
-			});
+    function () {
+        var url = buffer.URL;
+        if (options.get("yankencodedurl").value)
+            url = services.get("io").newURI(url, buffer.charser, null).asciiSpec;
+        else
+            url = services.get("textToSubURI").unEscapeURIForUI(buffer.charset, url).replace(/ /g, "%20");
+        node = url.split('/').pop()
+        util.copyToClipboard(node, true);
+            }
+);
 
-	
+    
